4f665b0c69c60e518ce85c1675db4dd60f107ea7
fish <I@fishfish.date>
2025-09-01 16:34:09 +0800

    feat(app): 添加命令行参数支持
    - 增加了 port 和 prod 两个命令行参数
    - 使用 fiber.Config 配置 Prefork 选项
    - 允许通过命令行指定监听端口和是否启用生产模式

--- app.go
+++ app.go
@@ -1,13 +1,24 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"flag"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+var (
+	port = flag.String("port", ":3000", "监听端口")
+	prod = flag.Bool("prod", false, "在生产环境中启用 prefork")
+)
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		Prefork: *prod, // go run app.go -prod
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Listen(":3000")
+	app.Listen(*port)
 }
